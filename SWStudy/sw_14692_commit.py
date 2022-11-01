08f476169d462143fc7f2924cc462eaca6318398
jinuemong <jinwoo3661@naver.com>
2022-11-01 17:54:58 +0900

    20221101

--- SWStudy/sw_14692_commit.py
+++ SWStudy/sw_14692_commit.py
@@ -5,3 +5,10 @@ for t in range(1,TC+1):
         print("#"+str(t),"Alice")
     else:
         print("#" + str(t), "Bob")
+
+
+# 출처
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AYJW0g-qlO8DFASv
+
+# 문제풀이
+# https://jinudmjournal.tistory.com/5
