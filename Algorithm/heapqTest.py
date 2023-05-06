6d334625b62970857b79289b652f4529df8225e8
김진우 <jinwoo3661@naver.com>
2023-05-06 20:40:15 +0900

    힙큐 스터디 5.6

---
+++ Algorithm/heapqTest.py
@@ -0,0 +1,14 @@
+
+# 최대 힙
+from heapq import heappush, heappop
+
+
+nums = [4, 1, 7, 3, 8, 5]
+heap = []
+
+for num in nums:
+  heappush(heap, (-num, num))  # (우선 순위, 값)
+
+while heap:
+  print(heappop(heap)[1])  # index 1
+
--- Baekjoon2/2250.py
+++ Baekjoon2/2250.py
@@ -75,3 +75,4 @@ for i in range(1,n+1):
             level = i
 
 print(level,width)
+  
