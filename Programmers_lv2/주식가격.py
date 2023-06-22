3d943bcc17aec478800761322850608dea55d6de
김진우 <jinwoo3661@naver.com>
2023-06-22 19:08:10 +0900

    6/22 스터디

---
+++ Programmers_lv2/주식가격.py
@@ -0,0 +1,22 @@
+
+# 주식 가격
+
+
+def solution(prices):
+    answer = []
+    return answer
+
+prices = [1, 2, 3, 2, 3]
+print(solution(prices))
+
+안떨어진 횟수 : 시작된 직후와 전은 안떨어짐
+s  1 2 3 2 3
+1  0 0 0 0 0
+2  1 0 0 0 0
+3  2 1 0 0 0
+2  3 1 0 0 0
+3  4 2 
+
+
+
+
