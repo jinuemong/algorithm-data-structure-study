1db959dd997dff4e0a2a9500a6d080e83a872dc3
jinuemong <jinwoo3661@naver.com>
2023-09-09 19:46:00 +0900

    lv2

--- Programmers_lv2_3/삼각 달팽이.py
+++ Programmers_lv2_3/삼각 달팽이.py
@@ -6,17 +6,15 @@
 # 맨 위 꼭짓점부터 반시계 방향으로 달팽이 채우기 진행
 
 def solution(n):
-    dicts = [[1,-1],[0,2],[-1,-1]]
-    answer,current , count= [[0]*(n*2) for _ in range(n*2)], [-1,n], 1
+    dicts = [[1,0],[0,1],[-1,-1]]
+    answer,current , count= [[0]*n for _ in range(n)], [-1,0], 1
 
     for i in range(n):
         for j in range(n-i):
             current[0]+=dicts[i%3][0]
             current[1]+=dicts[i%3][1]
             answer[current[0]][current[1]] = count
-            print(current)
             count+=1
-    print(answer)
-    return [answer[i][j] for i in range(n*2) for j in range(n*2) if answer[i][j]!=0]
+    return [answer[i][j] for i in range(n) for j in range(n) if answer[i][j]!=0]
 
-print(solution(3))
+print(solution(5))
