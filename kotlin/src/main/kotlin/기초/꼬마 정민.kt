353a187764df393309f1d3a4e1bd51576c3d4350
김진우 <jinwoo3661@naver.com>
2024-08-26 14:48:36 +0900

    8/26 스터디

--- kotlin/src/main/kotlin/기초/꼬마 정민.kt
+++ kotlin/src/main/kotlin/기초/꼬마 정민.kt
@@ -1,8 +1,8 @@
 package 기초
 
-fun main() = with(System.`in`.bufferedReader()){
-    val (a,b,c) = readln()
+fun main() = with(System.`in`.bufferedReader()) {
+    val (a, b, c) = readln()
         .split(" ")
         .map { it.toLong() }
-    print(a+b+c)
+    print(a + b + c)
 }
