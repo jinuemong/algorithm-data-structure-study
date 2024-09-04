7e56c1652e3ec5b5be476acd2d446ff8ad18ced7
김진우 <jinwoo3661@naver.com>
2024-09-04 09:34:42 +0900

    9/4 스터디

---
+++ kotlin/src/main/kotlin/기초/세로읽기.kt
@@ -0,0 +1,21 @@
+package 기초
+
+fun main() = with(System.`in`.bufferedReader()) {
+    var result = ""
+    val word = Array(5) { CharArray(15){' '} }
+    var maxSize = 0
+    repeat(5){i ->
+        val input  = readLine()
+        maxSize = Math.max(input.length,maxSize)
+        word[i] = input.toCharArray()
+    }
+
+    repeat(maxSize){ i ->
+        repeat(5){ j ->
+            word[j].getOrNull(i)?.let {
+                result += word[j][i]
+            }
+        }
+    }
+    print(result)
+}
