1e295d9b08af002ddf8c516556c7040c182c3a34
김진우 <jinwoo3661@naver.com>
2025-01-26 19:34:53 +0900

    1/26 스터디

---
+++ kotlin+java/src/main/kotlin+java/맥주 마시면서 걸어가기.kt
@@ -0,0 +1,40 @@
+import kotlin.math.abs
+
+fun main() {
+    val t = readlnOrNull()?.toInt() ?: return
+    repeat(t) {
+        val n = readlnOrNull()?.toInt() ?: return
+        val (sx, sy) = readlnOrNull()?.split(" ")?.map { it.toInt() } ?: return
+        val shop = mutableListOf<Pair<Int, Int>>()
+        repeat(n) {
+            val (x, y) = readlnOrNull()?.split(" ")?.map(String::toInt) ?: return
+            shop.add(x to y)
+        }
+        val (lx, ly) = readlnOrNull()?.split(" ")?.map { it.toInt() } ?: return
+
+        val deque = ArrayDeque<Pair<Int, Int>>()
+        deque.add(sx to sy)
+        val visited = mutableSetOf<Pair<Int, Int>>()
+
+        var isHappy = false
+        while (deque.isNotEmpty()) {
+            val (cx, cy) = deque.removeFirst()
+
+            // 목적지에 도달 가능한지 확인
+            if (abs(lx - cx) + abs(ly - cy) <= 1000) {
+                isHappy = true
+                break
+            }
+
+            // 편의점 탐색
+            shop.forEach {
+                if (it !in visited && abs(it.first - cx) + abs(it.second - cy) <= 1000) {
+                    visited.add(it)
+                    deque.add(it)
+                }
+            }
+        }
+
+        println(if (isHappy) "happy" else "sad")
+    }
+}
